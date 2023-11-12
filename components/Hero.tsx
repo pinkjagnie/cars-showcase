cfaeb305aeed75431c58e2e5e2414c7ebb2430ea
pinkjagnie <olga.krzysztofik@gmail.com>
2023-11-13 00:12:02 +0100

    Create CustomButton comp. & additional custom type

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import CustomButton from "./CustomButton";
+
 import Image from "next/image";
 
 const Hero = () => {
+  const handleScroll = () => {};
+
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -11,6 +17,11 @@ const Hero = () => {
           Streamline your car rental experience with our effortless booking
           process.
         </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
       </div>
     </div>
   );
