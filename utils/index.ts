ae8e87169c2c5768a56dabb6b86c634dd078f63f
pinkjagnie <olga.krzysztofik@gmail.com>
2023-11-13 21:44:04 +0100

    Develop of logic in Searchbar

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,4 @@
-import { FilterProps } from "@/types";
+import { FilterProps, CarProps } from "@/types";
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
   const basePricePerDay = 50; // Base rental price per day in dollars
@@ -37,3 +37,18 @@ export async function fetchCars(filters: FilterProps) {
 
   return result;
 }
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append("customer", "");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
+};
